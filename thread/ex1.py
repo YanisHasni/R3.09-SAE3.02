6789a34353f75f24e1c34ecb71aac510dc2892f3
YanisHasni <148784200+YanisHasni@users.noreply.github.com>
2024-11-04 14:36:46 +0000

    Thread ex1

---
+++ thread/ex1.py
@@ -0,0 +1,23 @@
+import threading
+import time
+
+def thread1():
+    for i in range (5):
+        print("Je suis la thread 1")
+        time.sleep(0.5)
+
+def thread2():
+    for i in range (5):
+        print("Je suis la thread 2")
+        time.sleep(0.5)
+
+t1 = threading.Thread(target=thread1)
+t1.start()
+
+t2 = threading.Thread(target=thread2)
+t2.start()
+
+t1.join()
+t2.join()
+
+
