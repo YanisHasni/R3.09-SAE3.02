29a6d3c55c081df72f82b4cbfbe988caf6c79278
YanisHasni <148784200+YanisHasni@users.noreply.github.com>
2024-11-04 15:42:35 +0000

    V4

--- thread/ex2.py
+++ thread/ex2.py
@@ -1,3 +1,21 @@
 import threading
 import time
 
+def countdown(name, start):
+    count = start
+    while count > 0:
+        print(f"{name} : {count}")
+        count -= 1
+        time.sleep(0.5) 
+
+
+thread1 = threading.Thread(target=countdown, args=("thread 1", 5))
+thread2 = threading.Thread(target=countdown, args=("thread 2", 3))
+
+
+thread1.start()
+thread2.start()
+
+
+thread1.join()
+thread2.join()
